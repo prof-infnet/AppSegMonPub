9dda704d6d3a4d245fa29ce7d3d518ce78befad8
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-10-17 10:51:15 -0300

    Permission Webview

--- app/src/main/java/br/edu/infnet/appsegmonpub/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/appsegmonpub/MainActivity.kt
@@ -3,13 +3,38 @@ package br.edu.infnet.appsegmonpub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import android.widget.ImageView
+import br.edu.infnet.appsegmonpub.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.webViewClient = WebViewClient()
+
+        binding.webView.loadUrl("https://www.infnet.edu.br/")
+
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (binding.webView.canGoBack())
+            binding.webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
     }
 
     fun callLoadImage(view: View) {
